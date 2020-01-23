87d128c02ac7b566ede74ffadf7e7c79311f4d15
Khalit <khalithartmann@gmail.com>
2020-01-23 13:03:18 +0100

    implemented route /conversations/:conversationId

--- server/src/conversation.controller.ts
+++ server/src/conversation.controller.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { MongoHelper } from './mongo.helper';
-import { ObjectId } from 'mongodb';
+import { ObjectId, ObjectID } from 'mongodb';
 import ConversationModel from './models/conversation.model';
 
 
@@ -29,29 +29,22 @@ conversationRouter.get('/users/:userId/conversations', function (req: express.Re
 
 conversationRouter.get('/conversations/:conversationId', function (req: express.Request, res: express.Response, next: express.NextFunction) {
     // TODO implement db access
-    res.json(
-        {
-            "conversationId": "dummy-1",
-            "name": "ChatmitA",
-            "member": [
-                { "userId": "dummy-a", "name": "Dummy Alice", "pictureURL": "" },
-                { "userId": "dummy-b", "name": "Dummy Bob", "pictureURL": "" }
-            ],
-            "messages": [
-                { "senderUserID": "dummy-a", "timestamp": "2020-01-15T21:26:17Z", "messageText": "Hallo Alice, ?" },
-                { "senderUserID": "dummy-b", "timestamp": "2020-01-15T21:40:24Z", "messageText": "Hey, hey" },
-                { "senderUserID": "dummy-a", "timestamp": "2020-01-15T21:55:24Z", "messageText": "Hast du schon was fuer das AMOS gemacht" },
-                { "senderUserID": "dummy-b", "timestamp": "2020-01-15T21:57:24Z", "messageText": "Ja ganz viel" },
-                { "senderUserID": "dummy-a", "timestamp": "2020-01-15T22:05:24Z", "messageText": "Oh echt, ich hab noch garnichts" }
-            ]
-        }
-    );
+    const collection = getCollection();
+    collection.findOne({
+        _id: new ObjectId(req.params.conversationId),
+    }).then((conv) => {
+        console.log(conv);
+        res.status(200).json(conv);
+
+    }).catch((error) => {
+        res.status(500).json({ "error": error });
+
+    });
 });
 
 
 // ### POST NewConversation --> conversation_id on 201
 conversationRouter.post('/conversation', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const members = req.body['members'];
     const collection = getCollection();
     const conversation = new ConversationModel(
         {
